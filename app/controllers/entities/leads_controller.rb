82641465a2abe6bbf70ceac84af44b23680e0fca
lumen <just.me.sober@gmail.com>
2015-12-01 23:45:46 +0200

    add lead conversion

--- app/controllers/entities/leads_controller.rb
+++ app/controllers/entities/leads_controller.rb
@@ -6,6 +6,20 @@
 class LeadsController < EntitiesController
   before_action :get_data_for_sidebar, only: :index
   autocomplete :account, :name, full: true
+  protect_from_forgery with: :exception, except: [:create]
+
+  prepend_before_filter :auth_user_before_filter, only: [:create]
+
+def auth_user_before_filter
+  if request.post? && !params[:authorization].blank? && !params[:token].blank?
+    user = User.find_by_password_hash_and_password_salt(params[:authorization], params[:token])
+    return unless user
+ # Implant @current_user so that :require_user filter becomes a noop.
+    params[:lead][:user_id] ||= user.id.to_s
+    instance_variable_set("@current_user", user)
+    logger.info(">>> web-to-lead: creating lead for user " + user.inspect)
+  end
+end
 
   # GET /leads
   #----------------------------------------------------------------------------
